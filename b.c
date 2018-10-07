1c891a47579c81810a28c61d4820ecf53de8fee7
amantayal44 <amant@iitk.ac.in>
2018-10-08 02:24:33 +0530

    added and modified

--- b.c
+++ b.c
@@ -3,7 +3,7 @@ int mainI()
 {
 int a,b;
 scanf("%d %d",&a,&b);
-int c = a*b;
+int c = a+b;
 printf("%d",c);
 return 0;
 }
--- c.c
+++ c.c
@@ -3,7 +3,6 @@ int main()
 {
 int a,b;
 scanf("%d%d",&a,&b);
-printf("product is %d",a*b);
-
+printf("%d",a-b);
 return 0;
 }
